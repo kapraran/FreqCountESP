37e4ad6febbb296f7d5e67efedcba22d180797e8
Nikos Kapraras <nikos@kapraran.dev>
2020-09-15 20:56:36 +0300

    updated README

--- examples/FreqCountLcd/FreqCountLcd.ino
+++ examples/FreqCountLcd/FreqCountLcd.ino
@@ -1,7 +1,7 @@
 #include "FreqCountESP.h"
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27); 
+LiquidCrystal_I2C lcd(0x27);
 
 int inputPin = 14;
 int timerMs = 1000;
@@ -9,9 +9,9 @@ int timerMs = 1000;
 void setup()
 {
   FreqCountESP.begin(inputPin, timerMs);
-  
+
   // setup lcd
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.clear();
   lcd.home();
 }
